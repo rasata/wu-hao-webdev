121739a3f26f7ba6aa909d378e1a8511c90d8019
wuhao4u <wuhao4u@gmail.com>
2017-02-28 23:39:13 -0500

    assignment 4: mostly done with image upload

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -1,22 +1,64 @@
 module.exports = function (app) {
+    var multer = require('multer'); // npm install multer --save
+    var upload = multer({ dest: __dirname+'/../../public/uploads' });
+
     app.post("/api/page/:pageId/widget", createWidget);
     app.get("/api/page/:pageId/widget", findAllWidgetsForPage);
     app.get("/api/widget/:widgetId", findWidgetById);
     app.put("/api/widget/:widgetId", updateWidget);
     app.delete("/api/widget/:widgetId", deleteWidget);
     app.put("/api/page/:pageId/widget?", reorderWidget);
+    app.post("/api/upload", upload.single('myFile'), uploadImage);
 
     var widgets = [
-        { "_id": "123", "index": 0, "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
-        { "_id": "234", "index": 1, "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-        { "_id": "345", "index": 2, "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-            "url": "http://lorempixel.com/400/200/"},
-        { "_id": "456", "index": 3, "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
-        { "_id": "567", "index": 4, "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-        { "_id": "678", "index": 5, "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-            "url": "https://youtu.be/AM2Ivdi9c4E" },
-        { "_id": "789", "index": 6, "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+        {"_id": "123", "index": 0, "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
+        {"_id": "234", "index": 1, "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+        {
+            "_id": "345", "index": 2, "widgetType": "IMAGE", "pageId": "321", "width": "100%",
+            "url": "http://lorempixel.com/400/200/"
+        },
+        {"_id": "456", "index": 3, "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
+        {"_id": "567", "index": 4, "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+        {
+            "_id": "678", "index": 5, "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+            "url": "https://youtu.be/AM2Ivdi9c4E"
+        },
+        {"_id": "789", "index": 6, "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
     ];
+
+    function uploadImage(req, res) {
+        // console.log("Hi mom, server's uploading img");
+        // console.log(req.query);
+        // console.log(req.params);
+        // console.log(req.body);
+
+        var widgetId = req.body.widgetId;
+        var width = req.body.width;
+        var myFile = req.file;
+
+        var originalname = myFile.originalname; // file name on user's computer
+        var filename = myFile.filename;     // new file name in upload folder
+        var path = myFile.path;         // full path of uploaded file
+        var destination = myFile.destination;  // folder where file is saved to
+        var size = myFile.size;
+        var mimetype = myFile.mimetype;
+
+        console.log(originalname);
+        console.log(filename);
+        console.log(path);
+        console.log(destination);
+        console.log(size);
+        console.log(mimetype);
+
+        var widget = widgets.find(function (w) {
+            return w._id == widgetId;
+        });
+
+        widget.url = __dirname+'/../../public/uploads/'+filename;
+        widget.width = width;
+
+        res.sendStatus(200);
+    }
 
     function reorderWidget(req, res) {
         // /api/page/:pageId/widget
@@ -64,7 +106,7 @@ module.exports = function (app) {
         var pageId = req.params.pageId;
 
         var ret = [];
-        for(var w in widgets) {
+        for (var w in widgets) {
             if (widgets[w].pageId == pageId) {
                 ret.push(widgets[w]);
             }
